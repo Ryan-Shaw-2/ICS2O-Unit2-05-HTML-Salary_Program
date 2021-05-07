be1726168de4de99873ab99ce1f8f18d258d6b82
RyanShaw21 <>
2021-05-07 15:56:22 +0000

    created index.html, style.css, script.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ICS2O-Unit2-05-HTML-Salary_Program
+[![GitHub's Super Linter](https://github.com/Ryan-Shaw-2/ICS2O-Unit2-05-HTML-Salary_Program/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Ryan-Shaw-2/ICS2O-Unit2-05-HTML-Salary_Program/actions)
+
+This website can be found at: [https://Ryan-Shaw-2.github.io/ICS2O-Unit2-05-HTML-Salary_Program](https://Ryan-Shaw-2.github.io/ICS2O-Unit2-05-HTML-Salary_Program)
---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 Ryan-Shaw-2 All rights reserved
+//
+// Created by: Ryan-Shaw-2
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateClicked () {
+  // this function calculates the users salary
+
+  // input
+  const hoursWorked = parseInt(document.getElementById("hours-worked").value)
+  const hourlyRate = parseInt(document.getElementById("hourly-rate").value)
+
+  // process
+
+
+  // output
+  
+}
